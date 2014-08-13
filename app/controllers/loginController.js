7292100d118bc449c94c554e0615ea727f9dea33
Rahul <rahul_ch@rahul_ch.PHOTON>
2014-08-13 15:31:51 +0530

    login added

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -3,4 +3,17 @@ app.controller("loginController", function($scope){
 		title:"Login", 
 		body:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage"
 	};
+	$scope.user = {
+		username : "",
+		password : ""
+	}
+	$scope.loginSubmit = function(isValid){
+		if(isValid){
+			//valid
+			alert(isValid);
+		}else{
+			//invalid
+			alert(isValid);
+		}
+	};
 });
